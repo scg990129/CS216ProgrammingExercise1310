239226b45897066c213a5e7d6c2e86739b3a753a
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-10-18 21:35:06 -0700

    build completed v1

--- fractionType.cpp
+++ fractionType.cpp
@@ -3,3 +3,73 @@
 //
 
 #include "fractionType.h"
+#include "iostream"
+#include <numeric>
+
+bool fractionType::shouldTheLowestTerms = false;
+
+fractionType::fractionType(int numerator, int denominator, bool simplification) {
+    if (simplification) {
+        fractionType ref = generateReducedFraction(numerator, denominator);
+        this->numerator = ref.numerator;
+        this->denominator = ref.denominator;
+    }else{
+        this->numerator = numerator;
+        this->denominator = denominator;
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const fractionType& fraction){
+    os << fraction.numerator << "/" << fraction.denominator;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, fractionType& fraction) {
+    is >> fraction.numerator;
+    is.ignore(1,'/');
+    is >> fraction.denominator;
+    return is;
+}
+
+fractionType fractionType::generateReducedFraction() const{
+    return generateReducedFraction(this->numerator, this->denominator);
+}
+
+fractionType fractionType::generateReducedFraction(int numerator, int denominator){
+    int gcd = std::gcd(numerator, denominator);
+    return {numerator/gcd, denominator/gcd, false};
+}
+fractionType fractionType::generateReducedFraction(const fractionType &fraction){
+    return generateReducedFraction(fraction.getNumerator(), fraction.getDenominator());
+}
+
+void fractionType::setFraction(int numerator, int denominator, bool simplification){
+    if (simplification) {
+        fractionType ref = generateReducedFraction(numerator, denominator);
+        this->numerator = ref.numerator;
+        this->denominator = ref.denominator;
+    }else{
+        this->numerator = numerator;
+        this->denominator = denominator;
+    }
+}
+
+int fractionType::getNumerator() const{
+    return numerator;
+}
+
+int fractionType::getDenominator() const{
+    return denominator;
+}
+
+bool operator == (const fractionType& fractionType1, const fractionType& fractionType2){
+    fractionType reducaedFractionType1 = fractionType1.generateReducedFraction();
+    fractionType reducaedFractionType2 = fractionType2.generateReducedFraction();
+    return (reducaedFractionType1.numerator == reducaedFractionType2.numerator && reducaedFractionType1.denominator == reducaedFractionType2.denominator);
+}
+
+bool operator != (const fractionType&, const fractionType&) ;
+bool operator <= (const fractionType&, const fractionType&) ;
+bool operator < (const fractionType&, const fractionType&) ;
+bool operator >= (const fractionType&, const fractionType&) ;
+bool operator > (const fractionType&, const fractionType&) ;
--- fractionType.h
+++ fractionType.h
@@ -5,9 +5,43 @@
 #ifndef CS216PROGRAMMINGEXERCISE1310_FRACTIONTYPE_H
 #define CS216PROGRAMMINGEXERCISE1310_FRACTIONTYPE_H
 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
 class fractionType {
+private:
+    static bool shouldTheLowestTerms;
+protected:
+    int numerator;
+    int denominator;
+    static fractionType generateReducedFraction(int numerator, int denominator);
+    static fractionType generateReducedFraction(const fractionType &fraction);
+public:
+    fractionType generateReducedFraction() const;
 
+    fractionType(int numerator = 0, int denominator = 1, bool simplification = shouldTheLowestTerms);
+    void setFraction(int numerator, int denominator, bool simplification = shouldTheLowestTerms);
+    int getNumerator() const;
+    int getDenominator() const;
+    fractionType operator+(const fractionType& otherFraction) const;
+    fractionType operator-(const fractionType& otherFraction) const;
+    fractionType operator*(const fractionType& otherFraction) const;
+    fractionType operator/(const fractionType& otherFraction) const;
+    //Overload the relational operators
+    friend bool operator == (const fractionType&, const fractionType&) ;
+    friend bool operator != (const fractionType&, const fractionType&) ;
+    friend bool operator <= (const fractionType&, const fractionType&) ;
+    friend bool operator < (const fractionType&, const fractionType&) ;
+    friend bool operator >= (const fractionType&, const fractionType&) ;
+    friend bool operator > (const fractionType&, const fractionType&) ;
+//    fractionType operator++();
+//    fractionType operator++(int);
+//    fractionType operator--();
+//    fractionType operator--(int);
+    friend std::ostream& operator<<(std::ostream& os, const fractionType& fraction);
+    friend std::istream& operator>>(std::istream& is, fractionType& fraction);
 };
-
 
 #endif //CS216PROGRAMMINGEXERCISE1310_FRACTIONTYPE_H
